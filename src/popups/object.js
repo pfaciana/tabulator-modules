680d2cd21724602a89056f4480a2531a8d5f4928
Phil Faciana <phil@renderdev.com>
2022-12-31 06:00:23 -0500

    add the extra `formatString` parameters to the object popup

--- src/popups/object.js
+++ src/popups/object.js
@@ -13,10 +13,11 @@ module.exports = function (e, component, onRendered) {
 	}
 
 	let formatterParams = {...(column.formatterParams ?? {})};
-	formatterParams.textLimit = formatterParams.popupTextLimit ?? false;
-	formatterParams.whiteSpace ??= 'pre';
-	formatterParams.htmlChars ??= false;
-	formatterParams.space ??= 4;
+	formatterParams.textLimit = formatterParams.popupTextLimit ?? formatterParams.textLimit ?? false;
+	formatterParams.moreText = formatterParams.popupMoreText ?? formatterParams.moreText ?? '...';
+	formatterParams.htmlChars = formatterParams.popupHtmlChars ?? formatterParams.htmlChars ?? false;
+	formatterParams.whiteSpace = formatterParams.popupWhiteSpace ?? formatterParams.whiteSpace ?? 'pre';
+	formatterParams.space = formatterParams.popupSpace ?? formatterParams.space ?? 4;
 
 	if (isObject(content)) {
 		content = JSON.stringify(content, null, formatterParams.space);
